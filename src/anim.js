b7d927ecfa00295b169802a3628c24b323de770b
rktmmnrn <rfanomezaniavo@gmail.com>
2025-01-07 16:47:18 +0300

    first with vite

---
+++ src/anim.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menutop = document.querySelector('.top-menu');
+  const nav = document.querySelector('.nav');
+  const stars = document.querySelectorAll('.stars');
+  const avistxtWrap = document.querySelectorAll('.avistxt-wrapper');
+  const avistxt = document.querySelector('.avistxt');
+  const btnAvisLeft = document.querySelector('.btnleft');
+  const btnAvisRight = document.querySelector('.btnright');
+  const spons = document.querySelector('.spons');
+
+  menutop.addEventListener('click', () => {
+    if (nav.classList.contains('active')) {
+      nav.classList.remove('active');
+      nav.style.transition = '200ms ease';
+      menutop.classList.remove('change');
+      menutop.style.transition = '200ms ease';
+    } else {
+      nav.classList.add('active');
+      menutop.classList.add('change');
+    }
+  })
+
+  stars.forEach((starElem) => {
+    starElem.addEventListener('click', () => {
+      starElem.style.color = 'yellow';
+    })
+  })
+
+  console.log(window.innerWidth); // largeur de l'écran
+  var widthContain = avistxt.clientWidth; //largeur de l'ecran actuel
+  console.log(widthContain);
+
+  btnAvisLeft.addEventListener('click', () => {
+    console.log('button left clicked...');
+    avistxt.scrollTo({left: 0})
+  })
+  btnAvisRight.addEventListener('click', () => {
+    console.log('button right clicked...');
+    avistxt.scrollTo({left: widthContain});
+  })
+
+  function scrolauto() {
+    setInterval(() => {
+      spons.scrollTo({left: spons.clientWidth});
+      console.log('interval');
+    }, 1000);
+    
+  }
+})
---
+++ src/main.js
@@ -0,0 +1,63 @@
+import './style.scss'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+// import animation from './anim.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menutop = document.querySelector('.top-menu');
+  const nav = document.querySelector('.nav');
+  const stars = document.querySelectorAll('.stars');
+  const avistxtWrap = document.querySelectorAll('.avistxt-wrapper');
+  const avistxt = document.querySelector('.avistxt');
+  const btnAvisLeft = document.querySelector('.btnleft');
+  const btnAvisRight = document.querySelector('.btnright');
+  const spons = document.querySelector('.spons');
+
+  btnAvisLeft.innerHTML = `<`
+  btnAvisRight.innerHTML = `>`
+
+  menutop.addEventListener('click', () => {
+    if (nav.classList.contains('active')) {
+      nav.classList.remove('active');
+      nav.style.transition = '200ms ease';
+      menutop.classList.remove('change');
+      menutop.style.transition = '200ms ease';
+    } else {
+      nav.classList.add('active');
+      menutop.classList.add('change');
+    }
+  })
+
+  stars.forEach((starElem) => {
+    starElem.addEventListener('click', () => {
+      starElem.style.color = 'yellow';
+    })
+  })
+
+  console.log(window.innerWidth); // largeur de l'écran
+  var widthContain = avistxt.clientWidth; //largeur de l'ecran actuel
+  console.log(widthContain);
+
+  btnAvisLeft.addEventListener('click', () => {
+    console.log('button left clicked...');
+    avistxt.scrollTo({left: 0})
+  })
+  btnAvisRight.addEventListener('click', () => {
+    console.log('button right clicked...');
+    avistxt.scrollTo({left: widthContain});
+  })
+
+  function scrolauto() {
+    setInterval(() => {
+      spons.scrollTo({left: spons.clientWidth});
+      console.log('interval');
+    }, 1000);
+    
+  }
+})
+
+document.querySelector('#app').innerHTML = `
+  
+`
+setupCounter(document.querySelector('#counter'))
